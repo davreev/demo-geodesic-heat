744394fbf739a89f4957b866420d28e2170d8f93
David Reeves <reevdav@gmail.com>
2026-05-12 01:53:08 +0100

    Update dr-app

--- cmake/deps/dr-app.cmake
+++ cmake/deps/dr-app.cmake
@@ -7,7 +7,10 @@ include(FetchContent)
 FetchContent_Declare(
     dr-app
     GIT_REPOSITORY https://github.com/davreev/dr-app.git
-    GIT_TAG b80c3f22181956baf37bb089ae04f0fbfd0e651e
+    GIT_TAG 5a08614926ea8e8b17f880eb985b1bfd12fffb78
 )
 
 FetchContent_MakeAvailable(dr-app)
+
+# DEBUG(dr): Use local dr-app
+# add_subdirectory("/home/dave/dev/dr-app" "${CMAKE_BINARY_DIR}/_deps/dr-app-local-build" )
--- src/renderer.cpp
+++ src/renderer.cpp
@@ -365,7 +365,7 @@ void emit_draw_cmds<ContourColorMaterial>(MeshPlot const& src, DrawContext& draw
     if (mat == nullptr)
         return;
 
-    auto set_bindings = [](DrawCommand const& cmd, GfxBindings& b) {
+    auto set_bindings = [](DrawCommand const& cmd, sg_bindings& b) {
         auto const geom = static_cast<Geometry const*>(cmd.geometry);
         b.vertex_buffers[0] = geom->vertex;
         b.vertex_buffers[1] = geom->vertex;
@@ -409,7 +409,7 @@ void emit_draw_cmds<ContourLineMaterial>(MeshPlot const& src, DrawContext& draw_
     if (mat == nullptr)
         return;
 
-    auto set_bindings = [](DrawCommand const& cmd, GfxBindings& b) {
+    auto set_bindings = [](DrawCommand const& cmd, sg_bindings& b) {
         auto const geom = static_cast<Geometry const*>(cmd.geometry);
         b.vertex_buffers[0] = geom->vertex;
         b.vertex_buffers[1] = geom->vertex;
